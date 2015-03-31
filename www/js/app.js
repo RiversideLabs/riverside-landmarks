96f74b204b9b824cbdec120a660ffc1ffa525f84
Mike Stecker <mike@stecker.me>
2015-03-31 15:46:51 -0700

    Added Credits per Teri Delcamp

--- www/js/app.js
+++ www/js/app.js
@@ -90,6 +90,15 @@ angular.module('landmarkConnect', ['ionic', 'ngAnimate', 'ngStorage', 'ngCordova
         }
       }
     })
+    
+    .state('m.credits', {
+      url: "/credits",
+      views: {
+        'menuContent2' :{
+          templateUrl: "templates/credits.html"
+        }
+      }
+    })
 
     // Each tab has its own nav history stack:
 
